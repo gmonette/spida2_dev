3eb115092ad2627a03fadabbce599128995a3138
benyamindsmith <benyamindsmith@gmail.com>
2019-02-12 17:08:45 -0500

    Added (TO DO: Georges)

--- R/ConjComp.R
+++ R/ConjComp.R
@@ -3,19 +3,22 @@
 ###
 
 #' Conjugate complement of span(X) in span(Z) with respect to inner product ip
+#' 
+#'ConjComp returns a basis for the conjugate complement of the
+#'conjugate projection of X into span(Z) with respect to inner product with
+#'matrix ip.
 #'
-#' @param X
-#' @param Z
-#' @param ip
-#' @param tol
-#' @export
-ConjComp <- function( X , Z = diag( nrow(X)) , ip = diag( nrow(X)), tol = 1e-07 ) {
-  help <- "
-  ConjComp returns a basis for the conjugate complement of the
-  conjugate projection of X into span(Z) with respect to inner product with
-  matrix ip.
-  Note: Z is assumed to be of full column rank but not necessarily X.
-  "
+#'Note: Z is assumed to be of full column rank but not necessarily X.
+#'
+#'@param X   (TO DO: Georges)
+#'@param Z   (TO DO: Georges)
+#'@param ip  (TO DO: Georges)
+#'@param tol (TO DO: Georges)
+#'
+#'@export
+ConjComp <- 
+  function( X , Z = diag( nrow(X)) , ip = diag( nrow(X)), tol = 1e-07 ) 
+    {
 
   xq <- qr(t(Z) %*% ip %*% X, tol = tol)
   if ( xq$rank == 0 ) return( Z )
@@ -25,9 +28,9 @@ ConjComp <- function( X , Z = diag( nrow(X)) , ip = diag( nrow(X)), tol = 1e-07 
 
 #' Orthogonal basis for the orthogonal complement of the column space of a matrix.
 #'
-#' @param X
-#' @param Z
-#' @param tol
+#'@param X   (TO DO: Georges)
+#'@param Z   (TO DO: Georges)
+#'@param tol (TO DO: Georges)
 #' @export
 OrthoComp <-
   function (X, Z , tol = 1e-07) {
